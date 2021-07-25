dbd0d233a389b142ce2bf9330d64474ecd285ddc
Yeojin <bluemeta@gmail.com>
2021-07-25 21:34:48 +0900

    Add DATABASES setting

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_playground',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': 'mysql',
     }
 }
 
